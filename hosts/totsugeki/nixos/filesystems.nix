a78efc678848bf7ec8e536072eec674d330b0731
Avery <aveeryy@protonmail.com>
2024-10-11 21:13:06 +0200

    Don't compress home and data drives

--- hosts/totsugeki/nixos/filesystems.nix
+++ hosts/totsugeki/nixos/filesystems.nix
@@ -14,13 +14,12 @@
     "/home" = {
       device = "/dev/disk/by-uuid/4bbd6139-7caa-4617-a94f-b185c5f6ca45";
       fsType = "btrfs";
-      options = [ "compress=zstd:15" ];
     };
 
     "/mnt/Datos" = {
       device = "/dev/disk/by-uuid/994ef2bd-a9fb-4414-9a0a-19b150ffa452";
       fsType = "btrfs";
-      options = [ "compress=zstd:15" "user" "x-systemd.automount" "exec" ];
+      options = [ "user" "x-systemd.automount" "exec" ];
     };
   };
 }
