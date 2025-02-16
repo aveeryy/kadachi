15ba95c26a7df4c3e292626821c94014845eb547
Avery <aveeryy@protonmail.com>
2025-02-16 17:17:46 +0100

    Use labels for filesystems in greatyamada

--- hosts/greatyamada/nixos/filesystems.nix
+++ hosts/greatyamada/nixos/filesystems.nix
@@ -1,39 +1,28 @@
 { ... }: {
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/61050e8d-41c6-4c37-98a9-d8b0cdce6903";
+      device = "/dev/disk/by-label/NIXROOT";
       fsType = "btrfs";
       options = [ "compress=zstd:15" ];
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/3397-A4FF";
+      device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
     };
-    "/home" = {
-      device = "/dev/disk/by-uuid/61050e8d-41c6-4c37-98a9-d8b0cdce6903";
-      fsType = "btrfs";
-      options = [ "compress=zstd:15" "subvol=/home" ];
-    };
     "/mnt/Datos" = {
-      device = "/dev/disk/by-uuid/6a5274fc-8ee8-41ae-b7a0-867e5bbc25f4";
+      device = "/dev/disk/by-label/Datos";
       fsType = "btrfs";
       options = [ "compress=zstd:15" ];
     };
     "/mnt/Datos/minecraft" = {
-      device = "/dev/disk/by-uuid/6a5274fc-8ee8-41ae-b7a0-867e5bbc25f4";
+      device = "/dev/disk/by-label/Datos";
       fsType = "btrfs";
       options = [ "compress=zstd:4" "subvol=/minecraft" ];
     };
     "/mnt/Datos/music" = {
-      device = "/dev/disk/by-uuid/6a5274fc-8ee8-41ae-b7a0-867e5bbc25f4";
+      device = "/dev/disk/by-label/Datos";
       fsType = "btrfs";
       options = [ "subvol=/music" ];
     };
-    "/var/www" = {
-      device = "/dev/disk/by-uuid/6a5274fc-8ee8-41ae-b7a0-867e5bbc25f4";
-      fsType = "btrfs";
-      options = [ "compress=zstd:15" "subvol=/html" ];
-    };
   };
-
 }
