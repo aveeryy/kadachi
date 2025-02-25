3930f32511a1913e619e2249e863901568ebdc9d
Avery <aveeryy@protonmail.com>
2025-02-25 12:48:31 +0000

    Install MongoDB on mizuki

--- hosts/mizuki/nixos.nix
+++ hosts/mizuki/nixos.nix
@@ -1,14 +1,17 @@
-{ config, lib, pkgs, ... }: {
+{ lib, pkgs, ... }: {
 
   environment.shells = with pkgs; [ zsh ];
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.config.allowUnfree = true;
 
   programs.zsh.enable = true;
 
   security.sudo.enable = true;
+
+  services = { mongodb.enable = true; };
 
   users = {
     defaultUserShell = pkgs.zsh;
