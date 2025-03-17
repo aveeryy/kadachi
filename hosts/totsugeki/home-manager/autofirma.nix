ea6143450d39f901922587e5ca4f4c854d69dc5f
Avery <aveeryy@protonmail.com>
2025-03-17 16:56:40 +0100

    Disable DNIeRemote

--- hosts/totsugeki/home-manager/autofirma.nix
+++ hosts/totsugeki/home-manager/autofirma.nix
@@ -1,17 +1,11 @@
-{ config, pkgs, ... }: {
+{ pkgs, ... }: {
   programs = {
     autofirma = {
       enable = true;
       firefoxIntegration.profiles.Avery.enable = true;
     };
-    dnieremote = {
-      enable = true;
-      usbPort = 9500;
-    };
     firefox.policies.SecurityDevices = {
       "OpenSC PKCS11" = "${pkgs.opensc}/lib/opensc-pkcs11.so";
-      "DNIeRemote" =
-        "${config.programs.dnieremote.finalPackage}/lib/libdnieremotepkcs11.so";
     };
   };
 }
