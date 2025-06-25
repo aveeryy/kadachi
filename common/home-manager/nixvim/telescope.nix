18154472aa388ebe1074b0422f22f5bf2c3b614f
Avery <aveeryy@protonmail.com>
2025-06-25 11:03:00 +0000

    Add live_grep to telescope config

--- common/home-manager/nixvim/telescope.nix
+++ common/home-manager/nixvim/telescope.nix
@@ -1,8 +1,10 @@
-{ ... }: {
+{ pkgs, ... }: {
+  home.packages = with pkgs; [ ripgrep ];
   programs.nixvim.plugins.telescope = {
     enable = true;
     keymaps = {
       "<leader>ff" = "find_files";
+      "<leader>fg" = "live_grep";
       "<leader>bb" = "buffers";
     };
   };
