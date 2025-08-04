3bff0ca1d2b45607d96513f60280f7e104a0d307
Avery <aveeryy@protonmail.com>
2025-08-04 13:45:43 +0200

    feat(nixvim): Add mdformat

--- common/home-manager/nixvim/none-ls.nix
+++ common/home-manager/nixvim/none-ls.nix
@@ -1,10 +1,15 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.nixvim.plugins.none-ls = {
     enable = true;
     sources = {
       formatting = {
         black.enable = true;
         dart_format.enable = true;
+        mdformat = {
+          enable = true;
+          package = pkgs.mdformat.withPlugins
+            (packages: with packages; [ mdformat-tables ]);
+        };
         nixfmt.enable = true;
         xmllint.enable = true;
       };
