394047810ccb37e4ae5ef35cd7a8a4671fc9ea40
Avery <aveeryy@protonmail.com>
2025-11-10 23:24:04 +0100

    fix(common): Don't handle lazygit theme with stylix

--- common/home-manager/development.nix
+++ common/home-manager/development.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   imports = [ ./nixvim ];
-  home = { packages = with pkgs; [ git-credential-manager gnupg pass ]; };
+  home = {
+    packages = with pkgs; [
+      git-credential-manager
+      gnupg
+      pass
+    ];
+  };
   programs = {
     git = {
       enable = true;
@@ -21,23 +28,7 @@
         signByDefault = true;
       };
     };
-    lazygit = {
-      enable = true;
-      settings = {
-        gui.theme = {
-          activeBorderColor = [ "#89b4fa" "bold" ];
-          inactiveBorderColor = [ "#a6adc8" ];
-          optionsTextColor = [ "#89b4fa" ];
-          selectedLineBgColor = [ "#313244" ];
-          selectedRangeBgColor = [ "#313244" ];
-          cherryPickedCommitBgColor = [ "#45475a" ];
-          cherryPickedCommitFgColor = [ "#89b4fa" ];
-          unstagedChangesColor = [ "#f38ba8" ];
-          defaultFgColor = [ "#cdd6f4" ];
-          searchingActiveBorderColor = [ "#f9e2af" ];
-        };
-      };
-    };
+    lazygit.enable = true;
   };
   services = {
     gpg-agent = {
--- common/home-manager/theme.nix
+++ common/home-manager/theme.nix
@@ -1,8 +1,24 @@
 { ... }:
 {
   imports = [ ../nixos/theme.nix ];
+  programs.lazygit.settings.gui.theme = {
+    activeBorderColor = [
+      "#89b4fa"
+      "bold"
+    ];
+    inactiveBorderColor = [ "#a6adc8" ];
+    optionsTextColor = [ "#89b4fa" ];
+    selectedLineBgColor = [ "#313244" ];
+    selectedRangeBgColor = [ "#313244" ];
+    cherryPickedCommitBgColor = [ "#45475a" ];
+    cherryPickedCommitFgColor = [ "#89b4fa" ];
+    unstagedChangesColor = [ "#f38ba8" ];
+    defaultFgColor = [ "#cdd6f4" ];
+    searchingActiveBorderColor = [ "#f9e2af" ];
+  };
   stylix = {
     targets = {
+      lazygit.enable = false;
       kitty.enable = false;
       hyprlock.enable = false;
       hyprland.enable = false;
