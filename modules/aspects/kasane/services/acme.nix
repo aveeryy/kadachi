9c862092df8fe529578655a72758472a5c9f496f
Avery <aveeryy@protonmail.com>
2026-03-10 22:04:55 +0000

    refactor(kasane/{acme,ddns}): Cleanup implementation

--- modules/aspects/kasane/services/acme.nix
+++ modules/aspects/kasane/services/acme.nix
@@ -1,26 +1,24 @@
 { __findFile, den, ... }:
+let
+  makeAspect =
+    serviceAspect:
+    den.lib.take.exactly (
+      { host }:
+      {
+        includes = [
+          <adachi/services/acme>
+          (serviceAspect host.services.baseHost)
+        ];
+        nixos.security.acme.defaults.email = host.services.email;
+      }
+    );
+in
 {
   kasane.services._.acme = {
     description = "Wrapper around adachi ACME configurations using the host values";
     provides = {
-      cloudflare = den.lib.take.exactly (
-        { host }:
-        {
-          includes = [
-            (<adachi/services/acme> host.services.email)
-            (<adachi/services/acme/cloudflare> host.services.baseHost)
-          ];
-        }
-      );
-      desec = den.lib.take.exactly (
-        { host }:
-        {
-          includes = [
-            (<adachi/services/acme> host.services.email)
-            (<adachi/services/acme/desec> host.services.baseHost)
-          ];
-        }
-      );
+      cloudflare = (makeAspect <adachi/services/acme/cloudflare>);
+      desec = (makeAspect <adachi/services/acme/desec>);
     };
   };
 }
--- modules/aspects/kasane/services/ddns.nix
+++ modules/aspects/kasane/services/ddns.nix
@@ -1,26 +1,23 @@
 { __findFile, den, ... }:
+let
+  makeAspect =
+    serviceAspect:
+    den.lib.take.exactly (
+      { host }:
+      {
+        includes = [
+          <adachi/services/ddns>
+          (serviceAspect host.services.baseHost)
+        ];
+      }
+    );
+in
 {
   kasane.services._.ddns = {
     description = "Wrapper around adachi DDNS configurations using the host values";
     provides = {
-      cloudflare = den.lib.take.exactly (
-        { host }:
-        {
-          includes = [
-            <adachi/services/ddns>
-            (<adachi/services/ddns/cloudflare> host.services.baseHost)
-          ];
-        }
-      );
-      desec = den.lib.take.exactly (
-        { host }:
-        {
-          includes = [
-            <adachi/services/ddns>
-            (<adachi/services/ddns/desec> host.services.baseHost)
-          ];
-        }
-      );
+      cloudflare = (makeAspect <adachi/services/ddns/cloudflare>);
+      desec = (makeAspect <adachi/services/ddns/desec>);
     };
   };
 }
