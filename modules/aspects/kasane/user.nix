31d4e637641396591b0a6cae1ddbbde503fd34e3
Avery <aveeryy@protonmail.com>
2026-03-22 22:05:38 +0100

    refactor(kasane/user): Get username from user

--- modules/aspects/kasane/user.nix
+++ modules/aspects/kasane/user.nix
@@ -1,55 +1,57 @@
 {
   __findFile,
   inputs,
+  lib,
   ...
 }:
-let
-  username = "avery";
-  name = "Avery";
-in
 {
-  kasane.base-user = <den.lib.parametric> {
-    includes = [
-      <den/primary-user>
+  kasane.base-user =
+    { user, ... }:
+    let
+      username = user.userName;
+    in
+    {
+      includes = [
+        <den/primary-user>
 
-      <kasane/neovim>
-      <kasane/tools/git>
-      <kasane/zsh>
-    ];
+        <kasane/neovim>
+        <kasane/tools/git>
+        <kasane/zsh>
+      ];
 
-    nixos =
-      { config, ... }:
-      {
-        services.openssh.settings.AllowUsers = [ username ];
-        sops.secrets."${username}_password" = {
-          sopsFile = "${inputs.secrets}/common.yaml";
-          owner = "root";
-          neededForUsers = true;
-        };
-        users.users."${username}" = {
-          description = name;
-          hashedPasswordFile = config.sops.secrets."${username}_password".path;
-          openssh.authorizedKeys.keys = [
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtRRFXoFER38RXhmvzUsckxd+eqcWO7B6oHW6oDbZf0 ${username}@totsugeki"
-            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGxbFhJuLDn1nGytao5RhBOyckMEQtpxrLJZR6G7Aax ${username}@mizuki"
-          ];
-        };
-      };
-
-    homeManager =
-      { pkgs, ... }:
-      {
-        home.packages = with pkgs; [ python3 ];
-        programs.fzf.enable = true;
-        programs.git = {
-          settings.user = {
-            name = name;
-            email = "aveeryy@protonmail.com";
+      nixos =
+        { config, ... }:
+        {
+          services.openssh.settings.AllowUsers = [ username ];
+          sops.secrets."${username}_password" = {
+            sopsFile = "${inputs.secrets}/common.yaml";
+            owner = "root";
+            neededForUsers = true;
           };
-          signing.key = "B684FD451B692E04";
+          users.users."${username}" = {
+            description = lib.strings.toSentenceCase username;
+            hashedPasswordFile = config.sops.secrets."${username}_password".path;
+            openssh.authorizedKeys.keys = [
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKtRRFXoFER38RXhmvzUsckxd+eqcWO7B6oHW6oDbZf0 ${username}@totsugeki"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILGxbFhJuLDn1nGytao5RhBOyckMEQtpxrLJZR6G7Aax ${username}@mizuki"
+            ];
+          };
         };
-      };
 
-    jovian.steam.user = username;
-  };
+      homeManager =
+        { pkgs, ... }:
+        {
+          home.packages = with pkgs; [ python3 ];
+          programs.fzf.enable = true;
+          programs.git = {
+            settings.user = {
+              name = lib.mkDefault (lib.strings.toSentenceCase username);
+              email = lib.mkDefault "aveeryy@protonmail.com";
+            };
+            signing.key = lib.mkDefault "B684FD451B692E04";
+          };
+        };
+
+      jovian.steam.user = username;
+    };
 }
