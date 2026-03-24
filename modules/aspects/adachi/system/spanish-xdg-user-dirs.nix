75995c712492944289d276a0cf2231b1cd40031d
Avery <aveeryy@protonmail.com>
2026-03-24 23:09:04 +0100

    feat(adachi/system): Add spanish-xdg-user-dirs

---
+++ modules/aspects/adachi/system/spanish-xdg-user-dirs.nix
@@ -0,0 +1,21 @@
+{ lib, ... }:
+{
+  adachi.system._.spanish-xdg-user-dirs = {
+    description = "Set home-manager.xdg.userDirs directories to Spanish";
+
+    homeManager =
+      { config, ... }:
+      {
+        xdg.userDirs = {
+          desktop = lib.mkDefault "${config.home.homeDirectory}/Escritorio";
+          documents = lib.mkDefault "${config.home.homeDirectory}/Documentos";
+          download = lib.mkDefault "${config.home.homeDirectory}/Descargas";
+          music = lib.mkDefault "${config.home.homeDirectory}/Música";
+          pictures = lib.mkDefault "${config.home.homeDirectory}/Imágenes";
+          publicShare = lib.mkDefault "${config.home.homeDirectory}/Público";
+          templates = lib.mkDefault "${config.home.homeDirectory}/Plantillas";
+          videos = lib.mkDefault "${config.home.homeDirectory}/Vídeos";
+        };
+      };
+  };
+}
