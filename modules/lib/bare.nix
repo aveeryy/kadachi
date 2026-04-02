598442180a93e7cdbaba47823e92661c49dbb985
Avery <aveeryy@protonmail.com>
2026-04-02 16:02:15 +0000

    fix(lib): Use recursiveMerge to create backup configuration

--- modules/lib/bare.nix
+++ modules/lib/bare.nix
@@ -13,48 +13,50 @@ let
 
   createBackupConfiguration' =
     backupName: host: borgmaticConfiguration:
-    {
-      archive_name_format = "{hostname}-${backupName}-{now:%Y-%m-%dT%H:%M:%S.%f}";
-      repositories = host.services.backups.repositories backupName;
-      encryption_passphrase = "{credential file /run/secrets/backups/password/${backupName}}";
-      ssh_command = "ssh -p 23 -i ${
-        self.nixosConfigurations.${host.hostName}.config.sops.templates."backups_ssh_private_key".path
-      }";
+    recursiveMerge ([
+      {
+        archive_name_format = "{hostname}-${backupName}-{now:%Y-%m-%dT%H:%M:%S.%f}";
+        repositories = host.services.backups.repositories backupName;
+        encryption_passphrase = "{credential file /run/secrets/backups/password/${backupName}}";
+        ssh_command = "ssh -p 23 -i ${
+          self.nixosConfigurations.${host.hostName}.config.sops.templates."backups_ssh_private_key".path
+        }";
 
-      ntfy = {
-        server = "https://ntfy.rcia.dev";
-        topic = "backups";
-        access_token = "{credential file /run/secrets/backups/ntfy_token}";
+        ntfy = {
+          server = "https://ntfy.rcia.dev";
+          topic = "backups";
+          access_token = "{credential file /run/secrets/backups/ntfy_token}";
 
-        finish = {
-          title = "Backup job finished";
-          message = "Backup job ${host.hostName}/${backupName} finished";
-          priority = "low";
-          tags =
-            if host.services.backups.identifyingIcon != "" then
-              "${host.services.backups.identifyingIcon},white_check_mark"
-            else
-              "white_check_mark";
+          finish = {
+            title = "Backup job finished";
+            message = "Backup job ${host.hostName}/${backupName} finished";
+            priority = "low";
+            tags =
+              if host.services.backups.identifyingIcon != "" then
+                "${host.services.backups.identifyingIcon},white_check_mark"
+              else
+                "white_check_mark";
+          };
+
+          fail = {
+            title = "Backup job failed";
+            message = "Backup job ${host.hostName}/${backupName} failed";
+            priority = "max";
+            tags =
+              if host.services.backups.identifyingIcon != "" then
+                "${host.services.backups.identifyingIcon},skull"
+              else
+                "skull";
+          };
+
+          states = [
+            "finish"
+            "fail"
+          ];
         };
-
-        fail = {
-          title = "Backup job failed";
-          message = "Backup job ${host.hostName}/${backupName} failed";
-          priority = "max";
-          tags =
-            if host.services.backups.identifyingIcon != "" then
-              "${host.services.backups.identifyingIcon},skull"
-            else
-              "skull";
-        };
-
-        states = [
-          "finish"
-          "fail"
-        ];
-      };
-    }
-    // borgmaticConfiguration;
+      }
+      borgmaticConfiguration
+    ]);
 
   isAttrSetEmpty = attrset: (lib.length (lib.attrsets.attrsToList attrset)) == 0;
 
@@ -71,6 +73,27 @@ let
     (map (aspect: aspect.provides.to-users) (
       filter (aspect: (hasAttr "provides" aspect) && (hasAttr "to-users" aspect.provides)) includedAspects
     ));
+
+  # Slighly modified version of https://stackoverflow.com/a/54505212
+  # Under the CC BY-SA 4.0 license: https://creativecommons.org/licenses/by-sa/4.0/
+  recursiveMerge =
+    attrList:
+    let
+      f =
+        attrPath:
+        lib.zipAttrsWith (
+          n: values:
+          if lib.tail values == [ ] then
+            lib.head values
+          else if lib.all lib.isList values then
+            lib.unique (lib.concatLists values)
+          else if lib.all lib.isAttrs values then
+            f (attrPath ++ [ n ]) values
+          else
+            lib.last values
+        );
+    in
+    f [ ] attrList;
 in
 {
   flake.lib = {
