9cf0cf965bce5e3d48c19411bc100f823a3d5dac
Avery <aveeryy@protonmail.com>
2026-04-21 11:11:10 +0200

    feat(mizuki): Add to Wireguard network

--- modules/hosts/mizuki/system.nix
+++ modules/hosts/mizuki/system.nix
@@ -1,13 +1,35 @@
 { __findFile, ... }:
 {
-  den.hosts.x86_64-linux.mizuki.users.avery = { };
+  den.hosts.x86_64-linux.mizuki = {
+    services.wireguard = {
+      nodeEnabled = true;
+      addresses = [ "10.10.0.3/16" ];
+      publicKey = "MbVmCHiEXd81GmmKl6lpy559o3Peho/4I0IbbOH8qU0=";
+    };
+    users.avery = { };
+  };
 
   den.aspects.mizuki = {
-    includes = [ <megurine/is/wsl> ];
+    includes = [
+      <adachi/services/podman>
+      <kasane/services/wireguard>
+      <megurine/is/wsl>
+    ];
 
-    nixos = {
-      time.timeZone = "Europe/Madrid";
-      virtualisation.docker.enable = true;
-    };
+    nixos =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = with pkgs; [
+          (podman-compose.overrideAttrs (_: {
+            src = fetchFromGitHub {
+              owner = "containers";
+              repo = "podman-compose";
+              rev = "ed3ec99699f692de5440c929ee9e7bfb116871c2";
+              hash = "sha256-OxuWnyxhT6sjH6K5b+7KTx4z8DLLn5sLFUDto9Pa6EY=";
+            };
+          }))
+        ];
+        time.timeZone = "Europe/Madrid";
+      };
   };
 }
