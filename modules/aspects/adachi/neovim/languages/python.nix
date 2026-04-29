0bd92c92259a3426f63dffef5fde800c6fab7e59
Avery <aveeryy@protonmail.com>
2026-04-29 17:31:48 +0200

    feat(neovim): Install ruff for Python diagnostics

--- modules/aspects/adachi/neovim/languages/python.nix
+++ modules/aspects/adachi/neovim/languages/python.nix
@@ -1,7 +1,17 @@
 { ... }:
 {
   adachi.neovim._.languages._.python.neovim.plugins = {
-    lsp.servers.basedpyright.enable = true;
+    lsp.servers = {
+      basedpyright = {
+        enable = true;
+        # Let ruff handle these diagnostics
+        settings.basedpyright.analysis.diagnosticSeverityOverrides = {
+          reportUnusedVariable = false;
+          reportUndefinedVariable = false;
+        };
+      };
+      ruff.enable = true;
+    };
     none-ls.sources.formatting.black.enable = true;
   };
 }
