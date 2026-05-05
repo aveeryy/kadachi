c247d9fff67d995ec0ca1d9824b97d2a46434326
Avery <aveeryy@protonmail.com>
2026-05-05 13:08:44 +0200

    feat(neovim): Highlight as regex in Python raw-strings

--- modules/aspects/adachi/neovim/languages/python.nix
+++ modules/aspects/adachi/neovim/languages/python.nix
@@ -1,17 +1,37 @@
 { ... }:
 {
-  adachi.neovim._.languages._.python.neovim.plugins = {
-    lsp.servers = {
-      basedpyright = {
-        enable = true;
-        # Let ruff handle these diagnostics
-        settings.basedpyright.analysis.diagnosticSeverityOverrides = {
-          reportUnusedVariable = false;
-          reportUndefinedVariable = false;
+  adachi.neovim._.languages._.python.neovim = {
+    plugins = {
+      lsp.servers = {
+        basedpyright = {
+          enable = true;
+          # Let ruff handle these diagnostics
+          settings.basedpyright.analysis.diagnosticSeverityOverrides = {
+            reportUnusedVariable = false;
+            reportUndefinedVariable = false;
+          };
         };
+        ruff.enable = true;
       };
-      ruff.enable = true;
+      none-ls.sources.formatting.black.enable = true;
     };
-    none-ls.sources.formatting.black.enable = true;
+    extraFiles."queries/python/injections.scm".text = /* query */ ''
+      ; extends
+
+      (assignment
+        right: (string
+          (string_start) @_ss
+          (string_content) @injection.content)
+        (#match? @_ss "[^rR]?[rR][^rR]?[\"']")
+        (#set! injection.language "regex"))
+
+      (assignment
+        right: (parenthesized_expression
+        	(string
+            (string_start) @_ss
+            (string_content) @injection.content))
+        (#match? @_ss "[^rR]?[rR][^rR]?[\"']")
+        (#set! injection.language "regex"))
+    '';
   };
 }
