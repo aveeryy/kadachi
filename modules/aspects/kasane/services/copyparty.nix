098fc1a2a30fe93a63ea21d20eae5316cbd34db4
Avery <aveeryy@protonmail.com>
2026-05-24 23:20:53 +0200

    feat(hatsune): More copyparty configuration

--- modules/aspects/kasane/services/copyparty.nix
+++ modules/aspects/kasane/services/copyparty.nix
@@ -41,23 +41,40 @@ in
           services = {
             copyparty = {
               enable = true;
+              settings = {
+                e2dsa = true;
+                e2ts = true;
+                shr = "/share";
+                rproxy = "1";
+              };
               accounts = genAttrs host.services.copyparty.accounts (name: {
                 passwordFile = config.sops.secrets."copyparty/users/${name}".path;
               });
             };
             nginx.virtualHosts."${host.services.copyparty.domain}" = {
-              locations."/".proxyPass = "http://localhost:3923";
+              locations = {
+                "/" = {
+                  proxyPass = "http://localhost:3923";
+                  extraConfig = "client_max_body_size 1G;";
+                };
+                # TODO: make these configurable depending on share configuration
+                "/.cpr".proxyPass = "http://localhost:3923";
+                "/share" = {
+                  proxyPass = "http://localhost:3923";
+                  extraConfig = "client_max_body_size 1G;";
+                };
+              };
               forceSSL = true;
               useACMEHost = host.services.baseDomain;
-              extraConfig = host.services.nginx.localServiceConfig;
             };
           };
 
           sops.secrets = genAttrs' host.services.copyparty.accounts (
             user: nameValuePair "copyparty/users/${user}" { owner = config.services.copyparty.user; }
           );
+
           users = {
-            groups.disk-write = { };
+            groups.disk-write.gid = 900;
             users.copyparty.extraGroups = [ "disk-write" ];
           };
         };
--- modules/hosts/hatsune/system.nix
+++ modules/hosts/hatsune/system.nix
@@ -1,4 +1,4 @@
-{ __findFile, ... }:
+{ __findFile, self, ... }:
 {
   den.hosts.x86_64-linux.hatsune = {
     services = {
@@ -6,62 +6,91 @@
       defaultDatabase = "postgres";
       email = "infra-host-hatsune@rcia.dev";
       copyparty.domain = "hatsune.rcia.dev";
+      nginx.localServiceConfig = ''
+        error_page 403 https://miku.hatsune.rcia.dev;
+        allow 10.0.0.0/16;
+        allow 10.10.0.0/16;
+        deny all;
+      '';
     };
     users.avery = { };
   };
 
-  den.aspects.hatsune = {
-    description = "UGREEN NAS (DXP4800 Pro) system configuration";
+  den.aspects.hatsune =
+    { host }:
+    {
+      description = "UGREEN NAS (DXP4800 Pro) system configuration";
 
-    includes = [
-      <megurine/has/intel-cpu>
-      <megurine/has/intel-cpu/kvm>
-      <megurine/is/server>
-      <megurine/requires/secure-boot>
+      includes = [
+        <megurine/has/intel-cpu>
+        <megurine/has/intel-cpu/kvm>
+        <megurine/is/server>
+        <megurine/requires/secure-boot>
 
-      <kasane/services/acme/desec>
-      <kasane/services/copyparty>
-      <kasane/services/ddns/desec>
-      <kasane/services/nginx>
-    ];
+        <kasane/services/acme/desec>
+        <kasane/services/copyparty>
+        <kasane/services/ddns/desec>
+        <kasane/services/nginx>
+      ];
 
-    nixos =
-      { config, ... }:
-      {
-        boot = {
-          extraModprobeConfig = ''
-            options it87 force_id=0x8613 ignore_resource_conflict=1
-          '';
-          extraModulePackages = with config.boot.kernelPackages; [ it87 ];
-          initrd.availableKernelModules = [
-            "xhci_pci"
-            "nvme"
-            "ahci"
-            "usb_storage"
-            "sd_mod"
-          ];
-          kernelModules = [ "it87" ];
-        };
+      nixos =
+        { config, ... }:
 
-        services.copyparty = {
-          settings = {
-            e2dsa = true;
+        let
+          commonVolumeFlags = {
+            # File system permissions
+            chmod_f = "664";
+            chmod_d = "775";
+            gid = config.users.groups.disk-write.gid;
           };
-          volumes = {
-            "/disk0" = {
-              path = "/mnt/disk0";
-              access = {
-                rwmda = [ "avery" ];
+        in
+        {
+          boot = {
+            extraModprobeConfig = ''
+              options it87 force_id=0x8613 ignore_resource_conflict=1
+            '';
+            extraModulePackages = with config.boot.kernelPackages; [ it87 ];
+            initrd.availableKernelModules = [
+              "xhci_pci"
+              "nvme"
+              "ahci"
+              "usb_storage"
+              "sd_mod"
+            ];
+            kernelModules = [ "it87" ];
+          };
+
+          services = {
+            copyparty = {
+              settings = {
+              };
+              volumes = {
+                "/disk0" = {
+                  path = "/mnt/disk0";
+                  access = {
+                    rwmda = [ "avery" ];
+                  };
+                  flags = commonVolumeFlags;
+                };
+                "/disk1" = {
+                  path = "/mnt/disk1";
+                  access = {
+                    rwmda = [ "avery" ];
+                  };
+                  flags = commonVolumeFlags;
+                };
               };
             };
-            "/disk1" = {
-              path = "/mnt/disk1";
-              access = {
-                rwmda = [ "avery" ];
+
+            nginx.virtualHosts."miku.${host.services.baseDomain}" = {
+              locations."= /" = {
+                root = "${self}/modules/assets";
+                tryFiles = "/migu.webp =404";
               };
+              forceSSL = true;
+              useACMEHost = host.services.baseDomain;
             };
           };
         };
-      };
-  };
+    };
 }
