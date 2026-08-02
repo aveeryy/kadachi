8ba23effc8385d89598fe51e6817b1d8ef630d38
Avery <aveeryy@protonmail.com>
2026-08-02 02:04:53 +0200

    obsidian: Replace symlinks with the actual file

--- modules/aspects/kasane/tools/obsidian.nix
+++ modules/aspects/kasane/tools/obsidian.nix
@@ -28,14 +28,33 @@ in
         vaultPaths = map (vault: vault.target) (attrValues config.programs.obsidian.vaults);
       in
       {
-        home.activation.ensureObsidianVaults = lib.hm.dag.entryAfter [ "writeBoundary" ] (
-          concatLines (map (path: ''run mkdir -p "$HOME/${path}"'') vaultPaths)
-        );
-        # home.packages = [
-        #   (inputs'.kadachi-nvim.packages.kadachi-nvim.override {
-        #     obsidianWorkspaces = [ ];
-        #   })
-        # ];
+        home.activation = {
+          ensureObsidianVaults = lib.hm.dag.entryAfter [ "writeBoundary" ] (
+            concatLines (
+              map (path: /* bash */ ''
+                run mkdir -p "$HOME/${path}"
+                run mkdir -p "$HOME/${path}/Diarias"
+              '') vaultPaths
+            )
+          );
+          # Syncthing doesn't support resolving symlinks, so when syncing to a non-NixOS device
+          # the symlink would be copied, which then the Obsidian app would discard and replace
+          # with an empty configuration file
+          replaceSymlinkWithFile = lib.hm.dag.entryAfter [ "linkGeneration" ] (
+            concatLines (
+              map (path: /* bash */ ''
+                for file in $(find "$HOME/${path}" -type l); do
+                  run cp -r $(readlink -e "$file") "''${file}.tmp" && rm -rf "$file" && mv "''${file}.tmp" "$file"
+                done
+                # Clean backup files
+                for file in $(find "$HOME/${path}" -name '*.bak'); do
+                  run chmod -R 777 "$file" && rm -rf "$file" 
+                done
+              '') vaultPaths
+            )
+          );
+
+        };
 
         programs.obsidian = {
           enable = true;
