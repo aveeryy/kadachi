0396914bee507bbf5c236d92ae58d154c52804d3
Avery <aveeryy@protonmail.com>
2026-08-09 13:35:22 +0200

    totsugeki: Overclock and undervolt GPU

---
+++ modules/hosts/totsugeki/gpu-tweaks.nix
@@ -0,0 +1,27 @@
+{ ... }: {
+  den.aspects.totsugeki.nixos = {
+    hardware.amdgpu.overdrive.enable = true;
+    services.lact = {
+      enable = true;
+      settings = {
+        version = 6;
+        daemon = {
+          log_level = "info";
+          admin_group = "wheel";
+          disable_clocks_cleanup = false;
+        };
+        apply_settings_timer = 5;
+        current_profile = null;
+        auto_switch_profiles = false;
+        gpus."1002:73DF-1DA2:E445-0000:08:00.0" = {
+          max_core_clock = 2800;
+          max_memory_clock = 1000;
+          voltage_offset = -100;
+          power_cap = 213.0;
+          performance_level = "auto";
+          fan_control_enabled = false;
+        };
+      };
+    };
+  };
+}
