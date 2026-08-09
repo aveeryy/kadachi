dba155a84b60679213ae4bd419e50b7ee933a828
Avery <aveeryy@protonmail.com>
2026-08-09 18:37:39 +0200

    totsugeki: Increase GPU voltage

--- modules/hosts/totsugeki/gpu-tweaks.nix
+++ modules/hosts/totsugeki/gpu-tweaks.nix
@@ -16,7 +16,7 @@
         gpus."1002:73DF-1DA2:E445-0000:08:00.0" = {
           max_core_clock = 2800;
           max_memory_clock = 1000;
-          voltage_offset = -100;
+          voltage_offset = -80;
           power_cap = 213.0;
           performance_level = "auto";
           fan_control_enabled = false;
