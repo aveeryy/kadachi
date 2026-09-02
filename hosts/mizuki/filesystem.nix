4b02b321af0ad8afec8207e6385918bb4278ae0e
Avery <aveeryy@protonmail.com>
2026-09-02 16:08:02 +0200

    mizuki: Enable BTRFS compression and disable access time on root

--- hosts/mizuki/filesystem.nix
+++ hosts/mizuki/filesystem.nix
@@ -4,6 +4,10 @@
       "/" = {
         device = "/dev/disk/by-label/NIXROOT";
         fsType = "btrfs";
+        options = [
+          "compress=zstd:15"
+          "noatime"
+        ];
       };
       "/boot" = {
         device = "/dev/disk/by-label/NIXBOOT";
