83c7fa9c3babfb31c1430c6a096273438f4c8fa7
Avery <aveeryy@protonmail.com>
2026-09-03 10:46:58 +0200

    mizuki/openvpn: Enable restart after sleep

--- hosts/mizuki/openvpn.nix
+++ hosts/mizuki/openvpn.nix
@@ -1,12 +1,9 @@
 { inputs, ... }: {
   hosts.mizuki = {
     nixos = { config, ... }: {
-      services.openvpn = {
-        restartAfterSleep = false;
-        servers.bisite = {
-          autoStart = false;
-          config = "config ${config.sops.secrets."bisite.ovpn".path}";
-        };
+      services.openvpn.servers.bisite = {
+        autoStart = false;
+        config = "config ${config.sops.secrets."bisite.ovpn".path}";
       };
 
       sops.secrets."bisite.ovpn" = {
